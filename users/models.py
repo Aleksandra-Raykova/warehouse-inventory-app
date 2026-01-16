91bfa736faab7a03e98f852f343672c7697b4771
Aleksandra Aleksandrova <a.d.raikova@gmail.com>
2026-01-16 16:25:59 +0200

    Add item and category models and corresponding tests

--- users/models.py
+++ users/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"Category: {self.name}"
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=50)
+    total_stock = models.PositiveIntegerField()
+    item_category = models.ForeignKey(to=Category, on_delete=models.PROTECT)
+
+    @property
+    def stock_status(self):
+        if self.total_stock == 0:
+            return "Out"
+        elif 0 < self.total_stock <= 5:
+            return "Low"
+        elif self.total_stock > 5:
+            return "OK"
+        else:
+            return "Invalid total stock"
+
+    def __str__(self):
+        return f"Item: {self.name}, {self.total_stock}, {self.item_category}"
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from users.models import Category, Item
 
-# Create your tests here.
+
+class ItemStockStatusTest(TestCase):
+    def setUp(self):
+        self.c = Category.objects.create(name='asd')
+
+    def test_item_has_zero_total_stock_should_return_out(self):
+        i = Item.objects.create(name='asd', total_stock=0, item_category=self.c)
+        self.assertEqual(i.stock_status, "Out")
+
+    def test_item_has_1_total_stock_should_return_low(self):
+        i = Item.objects.create(name='asd', total_stock=1, item_category=self.c)
+        self.assertEqual(i.stock_status, "Low")
+
+    def test_item_has_5_total_stock_should_return_low(self):
+        i = Item.objects.create(name='asd', total_stock=5, item_category=self.c)
+        self.assertEqual(i.stock_status, "Low")
+
+    def test_item_has_6_total_stock_should_return_ok(self):
+        i = Item.objects.create(name='asd', total_stock=6, item_category=self.c)
+        self.assertEqual(i.stock_status, "OK")
+
+    def test_item_has_negative_total_stock_should_return_invalid_total_stock(self):
+        i = Item.objects.create(name='asd', total_stock=6, item_category=self.c)
+        i.total_stock -= 7
+        self.assertEqual(i.stock_status, "Invalid total stock")
