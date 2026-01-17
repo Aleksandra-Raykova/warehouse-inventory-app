1617e6d1b24d95e3f746259dd147dfca02bb0017
Aleksandra Aleksandrova <a.d.raikova@gmail.com>
2026-01-17 15:50:59 +0200

    Create CustomUser model with username_field=email; CustomUserManager with create_user and create_superuser methods

--- users_app/models.py
+++ users_app/models.py
@@ -1,3 +1,55 @@
+from django.utils import timezone
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email=email, password=password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(
+        max_length=150,
+        unique=True,
+        help_text='Required. 150 characters or fewer.',
+        error_messages={"unique": "A user with that email already exists."},
+    )
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+
+    is_active = models.BooleanField(
+        default=True,
+        help_text="Designates whether this user should be treated as active. "
+                  "Unselect this instead of deleting accounts.",
+    ),
+
+    is_staff = models.BooleanField(
+        default=False,
+        help_text="Designates whether the user can log into this admin site.",
+    )
+
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
